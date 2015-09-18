d632c25759d5e64a0eed3ab309a9af7d2fe34c6c
Galileo Guzmán <guzgali@gmail.com>
2015-09-18 09:21:00 -0500

    Initial Commit

---
+++ QRImplementation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRImplementation
+//
+//  Created by Galileo Guzman on 18/09/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRImplementation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRImplementation
+//
+//  Created by Galileo Guzman on 18/09/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
